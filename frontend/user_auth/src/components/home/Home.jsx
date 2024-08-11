aa421ecee77dc8660167cafaca9d077b2be3120b
layelhajji <layelhajji3@gmail.com>
2024-08-11 12:35:02 +0100

    get users

--- frontend/user_auth/src/components/home/Home.jsx
+++ frontend/user_auth/src/components/home/Home.jsx
@@ -1,11 +1,51 @@
+import './home.css';
+import { useEffect, useState } from 'react';
 
-import './home.css'
 const Home = () => {
+  const [users, setUsers] = useState([]);
+  const token = localStorage.getItem("token");
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/api/users', {
+          headers: {
+            'Authorization': `Bearer ${token}`, // Remplacez par le token JWT
+          },
+        });
+        const data = await response.json();
+        setUsers(data);
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
   return (
     <div>
-      <p className='home'>welcome back!</p>
+      <h1 className='titre'>Users List</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => (
+            <tr key={user._id}>
+              <td>{user.firstName}</td>
+              <td>{user.lastName}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
